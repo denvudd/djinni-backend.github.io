bfb3e6a48fa2660f9013aa4dc4286e2cfdf21fae
Dmytro <dmitry.yurin2020@gmail.com>
2023-10-02 22:27:01 +0300

    add controller for update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { UpdateUserDto } from './dto/updateUserDto.dto';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +11,11 @@ export class UserController {
   @Get(':id')
   async findUser(@Param('id') id: string) {
     return await this.userService.findUserById(id);
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+    return await this.userService.update(id, dto);
   }
 }
