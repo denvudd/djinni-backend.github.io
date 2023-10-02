949b03b884e31378c794c01289b590472d60c067
Dmytro <dmitry.yurin2020@gmail.com>
2023-10-02 21:37:30 +0300

    adapt for Railway deploy

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5555);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
