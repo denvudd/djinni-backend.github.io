f0be4f1871ae19a1ce5cadca13b0903df179557a
Dmytro <dmitry.yurin2020@gmail.com>
2023-10-02 23:11:53 +0300

    add nullish for validate value in update user dto

--- src/user/dto/updateUserDto.dto.ts
+++ src/user/dto/updateUserDto.dto.ts
@@ -1,7 +1,8 @@
-import { IsString, IsUrl } from 'class-validator';
+import { IsString, IsUrl, ValidateIf } from 'class-validator';
 
 export class UpdateUserDto {
   @IsString()
   @IsUrl()
+  @ValidateIf((o) => o === null)
   avatar: string;
 }
